31a1cc53eeb28307b402c953cc654e044f73d3bf
jorstar <wlcjorstar@gmail.com>
2015-01-13 09:49:54 +0100

    login

--- HerkansingSysteemPVB/login.aspx.cs
+++ HerkansingSysteemPVB/login.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    herkansingDBEntities ef = new herkansingDBEntities();
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -18,12 +19,56 @@ public partial class _Default : System.Web.UI.Page
             string user = tbGebruikersnaam.Text;
             string passwd = Functies.CalculateHashedPassword(tbWachtwoord.Text, user);
 
-            var student
+            var beheer = ef.LoginBeheerder(user, passwd);
+            if (beheer.Any())
+            {
+                string role = "B";
+                Session["User"] = beheer.First();
+                Session["Role"] = role;
+
+                tbGebruikersnaam.Text = "";
+                tbWachtwoord.Text = "";
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                var docent = ef.LoginDocent(user, passwd);
+                if (docent.Any())
+                {
+                    string role = "D";
+                    Session["User"] = docent.First();
+                    Session["Role"] = role;
+
+                    tbGebruikersnaam.Text = "";
+                    tbWachtwoord.Text = "";
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    var student = ef.LoginStudent(user, passwd);
+                    if (student.Any())
+                    {
+                        string role = "S";
+                        Session["User"] = student.First();
+                        Session["Role"] = role;
+
+                        tbGebruikersnaam.Text = "";
+                        tbWachtwoord.Text = "";
+                        Response.Redirect("Home.aspx");
+                    }
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Gebruikersnaam of wachtwoord is onjuist ingevoerd.');", true);
+                        tbWachtwoord.Text = "";
+                        tbWachtwoord.Focus();
+                    }
+                }
+            }
 
         }
-        catch
+        catch (Exception ex)
         {
-
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + ex.Message + "');", true);
         }
     }
 }
