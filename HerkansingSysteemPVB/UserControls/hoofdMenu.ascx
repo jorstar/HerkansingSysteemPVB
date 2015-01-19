a2b19198ccd22ad915f6946cd649c63f7b65024b
ianno1993 <i.lol@hotmail.com>
2015-01-19 12:00:58 +0100

    Menu Fixed!

--- HerkansingSysteemPVB/UserControls/hoofdMenu.ascx
+++ HerkansingSysteemPVB/UserControls/hoofdMenu.ascx
@@ -1,4 +1,4 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="Menu.ascx.cs" Inherits="UserControlls_Menu" %>
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="hoofdMenu.ascx.cs" Inherits="UserControlls_Menu" %>
 <div id="menu" class="menu">
     <asp:Menu ID="Hoofdmenu" runat="server" EnableViewState="False" Orientation="Horizontal" DynamicEnableDefaultPopOutImage="False" StaticEnableDefaultPopOutImage="False">
         <Items>
---
+++ HerkansingSysteemPVB/UserControls/hoofdMenu.ascx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UserControlls_Menu : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (Convert.ToString(Session["Role"]) == "B")
+        {
+            MenuItem AanmakenDocent = Hoofdmenu.FindItem("AanmakenDocent");
+            Hoofdmenu.Items.Remove(AanmakenDocent);
+            MenuItem HerkansingOverzichtStudent = Hoofdmenu.FindItem("HerkansingOverzichtStudent");
+            Hoofdmenu.Items.Remove(HerkansingOverzichtStudent);
+            MenuItem AanmeldenHerkansing = Hoofdmenu.FindItem("AanmeldenHerkansing");
+            Hoofdmenu.Items.Remove(AanmeldenHerkansing);
+
+        }
+        else if(Convert.ToString(Session["Role"]) == "D")
+        {
+            MenuItem AanmakenBeheer = Hoofdmenu.FindItem("AanmakenBeheer");
+            Hoofdmenu.Items.Remove(AanmakenBeheer);
+            MenuItem AanmeldenHerkansing = Hoofdmenu.FindItem("AanmeldenHerkansing");
+            Hoofdmenu.Items.Remove(AanmeldenHerkansing);
+        }
+        else if (Convert.ToString(Session["Role"]) == "S")
+        {
+            MenuItem AanmakenDocent = Hoofdmenu.FindItem("AanmakenDocent");
+            Hoofdmenu.Items.Remove(AanmakenDocent);
+            MenuItem AanmakenBeheer = Hoofdmenu.FindItem("AanmakenBeheer");
+            Hoofdmenu.Items.Remove(AanmakenBeheer);
+        }
+        else
+        {
+            MenuItem AanmakenDocent = Hoofdmenu.FindItem("AanmakenDocent");
+            Hoofdmenu.Items.Remove(AanmakenDocent);
+            MenuItem AanmakenBeheer = Hoofdmenu.FindItem("AanmakenBeheer");
+            Hoofdmenu.Items.Remove(AanmakenBeheer);
+            MenuItem AanmeldenHerkansing = Hoofdmenu.FindItem("AanmeldenHerkansing");
+            Hoofdmenu.Items.Remove(AanmeldenHerkansing);
+            MenuItem HerkansingOverzichtStudent = Hoofdmenu.FindItem("HerkansingOverzichtStudent");
+            Hoofdmenu.Items.Remove(HerkansingOverzichtStudent);
+            MenuItem Home = Hoofdmenu.FindItem("Home");
+            Hoofdmenu.Items.Remove(Home);
+            MenuItem WachtwoordWijzigen = Hoofdmenu.FindItem("WachtwoordWijzigen");
+            Hoofdmenu.Items.Remove(WachtwoordWijzigen);
+            MenuItem Uitloggen = Hoofdmenu.FindItem("Uitloggen");
+            Hoofdmenu.Items.Remove(Uitloggen);
+        }
+    }
+}
