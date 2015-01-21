cd887ac58fd8688c6d4d2af55cb49429b0317190
ianno1993 <i.lol@hotmail.com>
2015-01-21 11:24:57 +0100

    Hoofd Menu Fixx

--- HerkansingSysteemPVB/UserControls/hoofdMenu.ascx.cs
+++ HerkansingSysteemPVB/UserControls/hoofdMenu.ascx.cs
@@ -9,8 +9,24 @@ public partial class UserControlls_Menu : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-        if (Convert.ToString(Session["Role"]) == "B")
+        if (Functies.CheckWWChange(Convert.ToString(Session["User"]), Convert.ToString(Session["Role"])))
+        {
+            MenuItem AanmakenDocent = Hoofdmenu.FindItem("AanmakenDocent");
+            Hoofdmenu.Items.Remove(AanmakenDocent);
+            MenuItem AanmakenBeheer = Hoofdmenu.FindItem("AanmakenBeheer");
+            Hoofdmenu.Items.Remove(AanmakenBeheer);
+            MenuItem AanmeldenHerkansing = Hoofdmenu.FindItem("AanmeldenHerkansing");
+            Hoofdmenu.Items.Remove(AanmeldenHerkansing);
+            MenuItem HerkansingOverzichtDocent = Hoofdmenu.FindItem("HerkansingOverzichtDocent");
+            Hoofdmenu.Items.Remove(HerkansingOverzichtDocent);
+            MenuItem Home = Hoofdmenu.FindItem("Home");
+            Hoofdmenu.Items.Remove(Home);
+            MenuItem WachtwoordWijzigen = Hoofdmenu.FindItem("WachtwoordWijzigen");
+            Hoofdmenu.Items.Remove(WachtwoordWijzigen);
+            MenuItem Uitloggen = Hoofdmenu.FindItem("Uitloggen");
+            Hoofdmenu.Items.Remove(Uitloggen);
+        }
+        else if (Convert.ToString(Session["Role"]) == "B")
         {
             MenuItem AanmakenDocent = Hoofdmenu.FindItem("AanmakenDocent");
             Hoofdmenu.Items.Remove(AanmakenDocent);
