6034f9d92c330baaa6e45535e6922b92a6cae99d
ianno1993 <i.lol@hotmail.com>
2015-01-21 10:56:51 +0100

    Nu de fucntie

--- HerkansingSysteemPVB/App_Code/Functies.cs
+++ HerkansingSysteemPVB/App_Code/Functies.cs
@@ -17,4 +17,19 @@ public class Functies
             return Convert.ToBase64String(computedHash);
         }
     }
+
+    public static bool CheckWWChange(string objSessionUser, string objSessionRole)
+    {
+        herkansingDBEntities entity = new herkansingDBEntities();
+
+        if (objSessionRole == "S")
+        {
+            return Convert.ToBoolean(entity.GetStudentFirstLogin(objSessionUser).First());
+        }
+        else if (objSessionRole == "D")
+        {
+            return Convert.ToBoolean(entity.GetDocentFirstLogin(objSessionUser).First());
+        }
+        else return false;
+    }
 }
